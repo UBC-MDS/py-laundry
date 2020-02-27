507ddab4b61db157b463921943c7ea75f0a01c8f
Cari <cari.gostic@gmail.com>
2020-02-26 17:14:24 -0800

    Add skeleton for categorize function

---
+++ py-laundry/py-laundry.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+def categorize(df):
+    """
+    Categorizes each column in a dataframe as 'numeric', 
+    'categorical' or 'text' and returns a dictionary
+    with a list of column names for each category.
+    """
+    pass
