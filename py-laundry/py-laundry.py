e4068dd74492b0711e47183c14e74642b1485222
garg1234 <gargkaman7@gmail.com>
2020-02-26 17:30:16 -0800

    Add Select Feature Function

--- py-laundry/py-laundry.py
+++ py-laundry/py-laundry.py
@@ -3,3 +3,12 @@ def fill_missing():
     Fill missing values in the dataframe based on user input.
     """
     return None
+
+
+def select_feature():
+
+    """
+    Select Important Features from the input data
+    """
+
+    return 0
