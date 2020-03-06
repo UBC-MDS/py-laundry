259a002e05caa578e2c8684d4abf00ae8d1602ff
Alexander Hinton <ahinton1991@gmail.com>
2020-03-06 09:09:28 -0800

    making naming convention consistent with other files

--- pylaundry/fill_missing.py
+++ pylaundry/fill_missing.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 ## TODO: Categorical features which are strings? ["A", "B", "C"]....isnull does not seem to be working? 
 
-def fill_missing(train_df, test_df, column_dict, num_trans, cat_trans):
+def fill_missing(X_train, X_test, column_dict, num_imp, cat_imp):
     """
     Fill missing values in the dataframe based on user input.
 
@@ -17,27 +17,28 @@ def fill_missing(train_df, test_df, column_dict, num_trans, cat_trans):
         A dictionary with keys = 'numeric','categorical',
         and values = a list of columns that fall into
         each respective category.
-    num_trans -- string(default -"mean")
+    num_imp -- string(default -"mean")
         imputation method for numeric features, options are "mean", "median"
-    cat_trans -- list
+    cat_imp -- list
         imputation method for categorical features, options are "mode"
 
     Returns
     -------
-    df_imputed-- pandas.core.frame.DataFrame  
-        A pandas dataframe
+    dictionary 
+        A dictionary with keys "X_train" and "X_test",
+        and the modified dataframes as values.
     
     """
         
     # Check input types are as specified
-    assert isinstance(train_df, pd.DataFrame), "train_df must be a Pandas DF"
-    assert isinstance(test_df, pd.DataFrame), "test_df must be a Pandas DF"
+    assert isinstance(X_train, pd.DataFrame), "X_train must be a Pandas DF"
+    assert isinstance(X_test, pd.DataFrame), "X_test must be a Pandas DF"
     assert isinstance(column_dict, dict), "column_dict must be a dictionary"
-    assert isinstance(num_trans, str), "num_trans should be a string"
-    assert isinstance(cat_trans, str), "cat_trans should be a string"
+    assert isinstance(num_imp, str), "num_imp should be a string"
+    assert isinstance(cat_imp, str), "cat_imp should be a string"
     
     # Check train set and test set columns are the same
-    assert np.array_equal(train_df.columns, test_df.columns), "train_df and test_df must have the same columns"
+    assert np.array_equal(X_train.columns, X_test.columns), "X_train and X_test must have the same columns"
     
     # Check dictionary keys are numeric and categorical
     for key in column_dict.keys():
@@ -47,47 +48,47 @@ def fill_missing(train_df, test_df, column_dict, num_trans, cat_trans):
     # Check all the columns listed in dictionary are in the df
     for keys, values in column_dict.items():
         for column in values:
-            assert column in train_df.columns, "columns in dictionary must be in dataframe"
+            assert column in X_train.columns, "columns in dictionary must be in dataframe"
             
     # Check that numerical imputation method is one of the two options 
-    assert num_trans == "mean" or num_trans == "median", \
+    assert num_imp == "mean" or num_imp == "median", \
         "numerical imputation method can only be mean or median"
     
     # Check that categorical imputation method is the only option
-    assert cat_trans == "mode", "cat_trans can only take 'mode' as argument value"
+    assert cat_imp == "mode", "cat_imp can only take 'mode' as argument value"
     
     # Imputation methods for numerical transforms
     for column in column_dict['numeric']:
         # get column mean or median
-        if num_trans == "mean":
-            col_imp = train_df[column].mean()
-        if num_trans == "median":
-            col_imp = train_df[column].median()
+        if num_imp == "mean":
+            col_imp = X_train[column].mean()
+        if num_imp == "median":
+            col_imp = X_train[column].median()
             
         # Get index of NaN values in train columns
         # Todo: If these are empty (no Nan) is that fine
-        index_train = train_df[column].index[train_df[column].apply(np.isnan)]
-        index_test = test_df[column].index[test_df[column].apply(np.isnan)]
+        index_train = X_train[column].index[X_train[column].apply(np.isnan)]
+        index_test = X_test[column].index[X_test[column].apply(np.isnan)]
         
         # Use impute value on train set
-        train_df.loc[index_train,column] = col_imp
+        X_train.loc[index_train,column] = col_imp
         # Use same impute value on test set
-        test_df.loc[index_test,column] = col_imp
+        X_test.loc[index_test,column] = col_imp
             
     
     # Imputation methods for categorical transforms 
     for column in column_dict['categorical']:
        # Note:  If mode is a tie, pandas picks lower value pick the lower value!
-        col_imp = train_df[column].mode()[0]
+        col_imp = X_train[column].mode()[0]
         
         # Get index of NaN values in train columns
-        index_train = train_df[column].index[train_df[column].isnull()]
-        index_test = test_df[column].index[test_df[column].isnull()]
+        index_train = X_train[column].index[X_train[column].isnull()]
+        index_test = X_test[column].index[X_test[column].isnull()]
         
         # Use impute value on train set
-        train_df.loc[index_train,column] = col_imp
+        X_train.loc[index_train,column] = col_imp
         # Use same impute value on test set
-        test_df.loc[index_test,column] = col_imp
+        X_test.loc[index_test,column] = col_imp
         
         
-    return (train_df, test_df)
+    return {"X_train": X_train, "X_test": X_test}
